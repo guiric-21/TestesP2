c51dcc58e20eb736d1fc01a6106f82535fa570a2
Mergs <gabrielsnmergulhao@gmail.com>
2024-07-02 00:04:40 -0300

    Adicionando testes para floats (Add, Subtract, Multiply, Divide)

--- atvTeste/src/test/java/br/upe/atvTeste/aritimeticaTests.java
+++ atvTeste/src/test/java/br/upe/atvTeste/aritimeticaTests.java
@@ -1,7 +1,22 @@
 package br.upe.atvTeste;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class aritimeticaTests {
+
+    DecimalFormat dFormat = new DecimalFormat("#, ###.###");
+    RoundingMode rmHalfUp = RoundingMode.HALF_UP;
+    BigDecimal bdNumE = new BigDecimal (5.2).setScale(1, rmHalfUp);
+    BigDecimal bdNumF = new BigDecimal (3.1).setScale(1, rmHalfUp);
+    BigDecimal bdNumG = new BigDecimal (8.3).setScale(1, rmHalfUp);
+    BigDecimal bdNumH = new BigDecimal (2.1).setScale(1, rmHalfUp);
+    BigDecimal bdNumI = new BigDecimal (16.12).setScale(2, rmHalfUp);
+    BigDecimal bdNumJ = new BigDecimal (1.6774193548).setScale(10, rmHalfUp);
+
     @Test
     void testAdd( ) {
         assertEquals(8,aritimetica.add(5,3));
@@ -26,4 +41,39 @@ public class aritimeticaTests {
     void testExponent( ) {
         assertEquals(125,aritimetica.exponent(5,3));
     }
+    @Test
+    void testAddFloat(){
+        assertEquals(bdNumG, aritimetica.add(bdNumE,bdNumF));
+
+    }
+    @Test
+    void testSubtractFloat(){
+        assertEquals(bdNumH, aritimetica.subtract(bdNumE,bdNumF));
+
+    }
+    @Test
+    void testMultiplyFloat(){
+        assertEquals(bdNumI, aritimetica.multiply(bdNumE,bdNumF));
+
+    }
+    @Test
+    void testDivideFloat(){
+        assertEquals(bdNumJ, aritimetica.divide(bdNumE,bdNumF,10, rmHalfUp));
+
+    }
+    /*@Test
+    void testExpoentFloat(){
+        assertEquals(165.81, dFormat.format(aritimetica.exponent(5.2,3.1)));
+
+    }
+    */
+
+
+
+
+
+
+
+
+
 }
